7a075f981f760e5e883f87c3fdb63273daf208fc
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-02-15 07:42:52 -0700

    Set up game loop

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 # Build a Bot to farm Coins in The Tower game.
 import pyautogui
 
+
 def main():
     pyautogui.sleep(3)
-    battle_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_Battle.png")
+    battle_btn = None
+    base_reset_time = 30
+
+
+    try:
+        battle_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_Battle.png", confidence=0.8)
+    except:
+        print("Battle Button not found...")
+
     if battle_btn != None:
         pyautogui.moveTo(battle_btn)
         pyautogui.click()
+
+
+    while True:
+        retry_btn = None
+        try:
+            retry_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_Retry.png", confidence=0.8)
+        except:
+            print("Retry Button not found. Sleeping...")
+
+        if retry_btn != None:
+            pyautogui.moveTo(retry_btn)
+            pyautogui.click()
+  
+
+            print("Game Restarting!")
+
+        pyautogui.sleep(base_reset_time)
 
 
 
