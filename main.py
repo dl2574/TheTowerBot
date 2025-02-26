5c75f7e9130afb657cd44c49a025525bfa6dfa10
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-02-26 05:19:45 -0700

    Refactor repetative code into new functions. find_btn and click_btn

--- main.py
+++ main.py
@@ -4,57 +4,41 @@ import pyautogui
 
 def main():
     pyautogui.sleep(3)
-    battle_btn = None
     base_reset_time = 30
 
-
-    try:
-        battle_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_Battle.png", confidence=0.8)
-    except:
-        print("Battle Button not found...")
+    battle_btn = find_btn("./assets/images/btn_Battle.png", "Battle Button not found...")
 
     if battle_btn != None:
-        pyautogui.moveTo(battle_btn)
-        pyautogui.click()
+        click_btn(battle_btn)
 
 
     while True:
-        gem_btn = None
-        float_btn = None
-        retry_btn = None
-
-        try:
-            gem_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_5_gem.png", confidence=0.8)
-        except:
-            print("No Gems.")
-
-        try:
-            float_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_float_gem.png", confidence=0.8)
-        except:
-            print("No Gems.")
-
-        try:
-            retry_btn = pyautogui.locateCenterOnScreen("./assets/images/btn_Retry.png", confidence=0.8)  
-        except:
-            print("Retry Button not found. Sleeping...")
+        gem_btn = find_btn("./assets/images/btn_5_gem.png", "No Gems.")
+        float_btn = find_btn("./assets/images/btn_float_gem.png", "No Gems.")
+        retry_btn = find_btn("./assets/images/btn_Retry.png", "Retry Button not found. Sleeping...")
 
         if gem_btn != None:
-            pyautogui.moveTo(gem_btn)
-            pyautogui.click()
+            click_btn(gem_btn)
 
         if float_btn != None:
-            pyautogui.moveTo(float_btn)
-            pyautogui.click()
-    
+            click_btn(float_btn)
+            
         if retry_btn != None:
-            pyautogui.moveTo(retry_btn)
-            pyautogui.click()
-  
-
+            click_btn(retry_btn)
             print("Game Restarting!")
 
         pyautogui.sleep(base_reset_time)
 
+def find_btn(img, message):
+    try:
+        return pyautogui.locateCenterOnScreen(img, confidence=0.8)
+    except:
+        print(message)
+        return None
+
+def click_btn(btn):
+    pyautogui.moveTo(btn)
+    pyautogui.click()
 
 
 if __name__ == "__main__":
