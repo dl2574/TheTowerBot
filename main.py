1d27a62e0e18fe03bfb017c0f406840c38f62897
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-03-08 07:55:31 -0700

    Upgrade health every 30 seconds.

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ def main():
     pyautogui.sleep(3)
     base_reset_time = 30
 
-    battle_btn = find_btn("./assets/images/btn_Battle.png", "Battle Button not found...")
+    battle_btn = find_img("./assets/images/btn_Battle.png", "Battle Button not found...")
+    defense_btn = find_img("./assets/images/btn_defense_tab.png", "No Defense Tab, Has the game started?")
 
     if battle_btn != None:
         click_btn(battle_btn)
 
 
+
     while True:
-        gem_btn = find_btn("./assets/images/btn_5_gem.png", "No Gems.")
-        float_btn = find_btn("./assets/images/btn_float_gem.png", "No Gems.")
-        retry_btn = find_btn("./assets/images/btn_Retry.png", "Retry Button not found. Sleeping...")
+        gem_btn = find_img("./assets/images/btn_5_gem.png", "No Gems.")
+        float_btn = find_img("./assets/images/btn_float_gem.png", "No Gems.")
+        retry_btn = find_img("./assets/images/btn_Retry.png", "Retry Button not found. Sleeping...")
+        
 
         if gem_btn != None:
             click_btn(gem_btn)
@@ -27,11 +30,22 @@ def main():
             click_btn(retry_btn)
             print("Game Restarting!")
 
+        if defense_btn != None:
+            defense_lbl = find_img("./assets/images/label_defense_upgrades.png", "Switching to the defense tab...")
+            if defense_lbl == None:
+                click_btn(defense_btn)
+                pyautogui.move(0, -160)
+            else:
+                pyautogui.moveTo(3180, 470)
+            pyautogui.scroll(100)
+            pyautogui.click()         
+
         pyautogui.sleep(base_reset_time)
 
-def find_btn(img, message):
+def find_img(img, message):
     try:
-        return pyautogui.locateCenterOnScreen(img, confidence=0.8)
+        # Top right region of the screen on my large monitor
+        return pyautogui.locateCenterOnScreen(img, confidence=0.8, region=(3000,0,438,660))
     except:
         print(message)
         return None
---
+++ scroll_test.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+
+pyautogui.sleep(3)
+pyautogui.click(button="middle")
+pyautogui.PAUSE = 2.5
+pyautogui.scroll(5000)
